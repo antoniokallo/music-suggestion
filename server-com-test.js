914b6f8d9e6b34e64a5150891b1b50973327ea7e
antoniokallo <antoniokallo@gmail.com>
2022-08-03 16:04:17 -0400

    new commit

--- server-com-test.js
+++ server-com-test.js
@@ -1,41 +1,48 @@
 
-const dropDown = document.querySelector('#dropdown')
-const detailedInfo = document.querySelector("#detailed-info")
-const music = document.querySelector('#music')
+/*what to accomplish 
+  1. make list clickable 
+  2. have songs show up when clicked
+  3. make a reset button 
+  4. have like button
 
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'aa7757373fmsh45a943009fead9ep12781fjsnb61ff5fdacaf',
-		'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
-	}
-};
+*/
+document.addEventListener('DOMContentLoaded', () => {
 
-fetch('https://deezerdevs-deezer.p.rapidapi.com/playlist/1677006641', options)
-	.then(response => response.json())
-	.then( data => {
-		data.array.forEach(tracks => {
-			const nameSpan = document.createElement("span");
-			nameSpan.innerText = tracks.name;
-	  
-			
-	  animalBar.append(nameSpan);
-			nameSpan.addEventListener("click", () => {
-	  
-			  music.textContent = tracks.name;
-			  animalImg.setAttribute("src", tracks.image);
-			});
-		  });
-		}
-		);
-	
+const Url = "https://api.deezer.com/playlist/10584916042"
 
+//fetch. use data.tracks.data to jump directly to the song
 
-	
+fetch(Url)
+	.then(res => res.json())
+  .then(data => {
+    data.tracks.data.forEach((song) => {
+      console.log(song.title)
+      renderSong(song)});
+    
+  })
 
+// make each click display xyz amount of songs
 
-// dropDown = document.addEventListener('click', event => {
+  function renderSong(song) {
+    
+    const details = document.querySelector('#detailed-info')
+    const h1 = document.createElement('h1')
 
+    details.append(h1)
 
+    h1.innerText = song.title
 
-// })
+    // let genreOne = document.querySelector('#Jazz')
+ 
+  //   genreOne.addEventListener('click', (e) => {
+  //     const details = document.querySelector('#detailed-info')
+  //     const songName = document.createElement('h1')
+  //       songName.append(details)
+  //     songName.innerText = song.song.title
+
+  // }
+  
+  //   )}
+
+  }
+   })
